a50c6054694b19b3278ffe6a9a25f46c34c693cd
Aidan-K-ACC <aidan.kroeger@g.austincc.edu>
2025-03-12 01:31:46 +0000

    q1 and q2 main functions

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,1 +1,17 @@
-#add import
+def get_age():
+    age= float(input("Please Enter Your Age.\n"))
+    return age
+
+def eval_age(age):
+    if age<0 or age>125:
+        print("invalid number")
+    else:
+        if age<=1:
+            print("infant")
+        if age>1 and age<13:
+            print("child")
+        if age>=13 and age<20:
+            print("teenager")
+        if age>=20:
+            print("adult")
+
